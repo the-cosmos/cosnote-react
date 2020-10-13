715b670ed57395e3f8180ca1086127490c50ee59
thec0sm0s <deepakrajko14@gmail.com>
2020-10-13 23:58:31 +0530

    Add base components for Header navigation and branding

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,42 @@
+import { FontSizes, IconButton, Stack } from '@fluentui/react';
 import React, {Component} from 'react';
+import NavigationBar from './Navigation';
+
+const navItemStyles = {
+    alignItems: 'center',
+    display: 'flex',
+    height: 50,
+    justifyContent: 'center',
+    width: 50,
+}
+
+class Brand extends Component {
+    render() {
+        return (
+            <div>
+                <Stack tokens={{childrenGap: 5}}>
+                    <Stack horizontal horizontalAlign="start">
+                        <span style={navItemStyles}>
+                            <IconButton iconProps={{iconName: "waffle", style: {fontSize: FontSizes.size68}} } />
+                        </span>
+                        <span style={navItemStyles}>
+                            <div className="brandingContainer">
+                                {/* <h1 style={brandBaseStyle}>Cosnote</h1> */}
+                            </div>
+                        </span>
+                    </Stack>
+                </Stack>
+            </div>
+        )
+    }
+}
 
 class Header extends Component {
     render() {
         return (
             <header>
-                
+                <Brand />
+                <NavigationBar />
             </header>
         );
     }
