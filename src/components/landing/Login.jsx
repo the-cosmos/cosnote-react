edf6695d2a9484c23c066c4bc0a2dd48c3cdb648
thec0sm0s <deepakrajko14@gmail.com>
2020-10-14 16:44:22 +0530

    Make image background cover of the div

--- src/components/landing/Login.jsx
+++ src/components/landing/Login.jsx
@@ -1,50 +1,54 @@
-// import { Stack } from '@fluentui/react'
+import { Stack } from '@fluentui/react'
 import { ImageFit, Image } from '@fluentui/react'
 import React, { Component } from 'react'
 
 
-// const stackStyles = {
-//     root: {
-//         // background: DefaultPalette.themeTertiary,
-//         overflow: 'hidden',
-//         width: "100%",
-//       },
-// }
+const stackStyles = {
+    root: {
+        // background: DefaultPalette.themeTertiary,
+        overflow: 'hidden',
+        width: "100%",
+      },
+}
 
-// const stackItemStyles = {
-//     root: {
-//         alignItems: 'center',
-//         // background: DefaultPalette.themePrimary,
-//         // color: DefaultPalette.white,
-//         display: 'flex',
-//         height: 50,
-//         justifyContent: 'center',
-//         overflow: 'hidden',
-//       },
-// }
+const stackItemStyles = {
+    root: {
+        alignItems: 'center',
+        // background: DefaultPalette.themePrimary,
+        // color: DefaultPalette.white,
+        display: 'flex',
+        height: 50,
+        justifyContent: 'center',
+        overflow: 'hidden',
+      },
+}
 
-// const nonShrinkingStackItemStyles = {
-//     root: {
-//       alignItems: 'center',
-//     //   background: DefaultPalette.themePrimary,
-//     //   color: DefaultPalette.white,
-//       display: 'flex',
-//       height: 50,
-//       justifyContent: 'center',
-//       overflow: 'hidden',
-//       width: 500,
-//     },
-// }
+const nonShrinkingStackItemStyles = {
+    root: {
+      alignItems: 'center',
+    //   background: DefaultPalette.themePrimary,
+    //   color: DefaultPalette.white,
+      display: 'flex',
+      height: 50,
+      justifyContent: 'center',
+      overflow: 'hidden',
+      width: 500,
+    },
+}
 
 export default class Login extends Component {
     render() {
         return (
-            <div className="workspace">
+            <div className="workspace loginCover">
+                {/* <Image src="/static/images/loginBackground.jpg" imageFit={ImageFit.cover} maximizeFrame /> */}
                 {/* <Stack horizontal styles={stackStyles} tokens={{childrenGap: 5}}>
-                    <Stack.Item grow styles={stackItemStyles}></Stack.Item>
-                    <Stack.Item grow disableShrink styles={nonShrinkingStackItemStyles}></Stack.Item>
+                    <Stack.Item grow styles={stackItemStyles}>
+                        ITEM 1
+                    </Stack.Item>
+                    <Stack.Item grow disableShrink styles={nonShrinkingStackItemStyles}>
+                        ITEm 2
+                    </Stack.Item>
                 </Stack> */}
-                <Image src="/static/images/loginBackground.jpg" imageFit={ImageFit.cover} maximizeFrame/>
             </div>
         )
     }
