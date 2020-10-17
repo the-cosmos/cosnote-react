f0f4b56c20b2c19d813d9cb58c3848b6d7884b66
thec0sm0s <deepakrajko14@gmail.com>
2020-10-17 22:15:43 +0530

    Add shimmer utility

---
+++ src/components/utils/Shimmer.jsx
@@ -0,0 +1,53 @@
+import { mergeStyles, ShimmerElementType } from '@fluentui/react'
+
+const shimmerWrapperClass = mergeStyles({
+    padding: 2,
+    selectors: {
+      '& > .ms-Shimmer-container': {
+        margin: 10,
+      },
+    },
+});
+
+const shimmerWithElementFirstRow = [
+    // { type: ShimmerElementType.circle },
+    // { type: ShimmerElementType.gap, width: '2%' },
+    { type: ShimmerElementType.line },
+];
+const shimmerWithElementSecondRow = [
+    // { type: ShimmerElementType.circle, height: 24 },
+    // { type: ShimmerElementType.gap, width: '2%' },
+    { type: ShimmerElementType.line, height: 16, width: '20%' },
+    { type: ShimmerElementType.gap, width: '5%' },
+    { type: ShimmerElementType.line, height: 16, width: '20%' },
+    // { type: ShimmerElementType.gap, width: '10%' },
+    { type: ShimmerElementType.line, height: 16, width: '15%' },
+    { type: ShimmerElementType.gap, width: '10%' },
+    { type: ShimmerElementType.line, height: 16 },
+];
+const shimmerWithElementThirdRow = [
+    // { type: ShimmerElementType.circle, height: 24 },
+    // { type: ShimmerElementType.gap, width: '2%' },
+    { type: ShimmerElementType.line, height: 16, width: '20%' },
+    { type: ShimmerElementType.gap, width: '5%' },
+    { type: ShimmerElementType.line, height: 16, width: '20%' },
+    { type: ShimmerElementType.gap, width: '10%' },
+    { type: ShimmerElementType.line, height: 16, width: '15%' },
+    { type: ShimmerElementType.gap, width: '10%' },
+    { type: ShimmerElementType.line, height: 16 },
+];
+
+
+export default class Shimmer extends Component {
+    render() {
+        return (
+            <div>
+                <div className={shimmerWrapperClass}>
+                    <Shimmer shimmerElements={shimmerWithElementFirstRow} />
+                    <Shimmer shimmerElements={shimmerWithElementSecondRow} />
+                    <Shimmer width="70%" shimmerElements={shimmerWithElementThirdRow} />
+                </div>
+            </div>
+        )
+    }
+}
