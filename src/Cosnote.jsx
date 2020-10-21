98aaaf8f54c6bf0acc55d08088a3fea5107cfe06
thec0sm0s <deepakrajko14@gmail.com>
2020-10-21 16:57:49 +0530

    Add method to attempt login from session

--- src/Cosnote.jsx
+++ src/Cosnote.jsx
@@ -2,7 +2,7 @@ import { Fabric, initializeIcons } from '@fluentui/react';
 import { ThemeProvider } from "@fluentui/react-theme-provider";
 import React, {Component} from 'react';
 import Header from "./components/header/header";
-// import Login from './components/landing/Login';
+import Login from './components/landing/Login';
 import Main from "./components/main/Main";
 import { CosnoteTheme } from './cosnoteTheme';
 import './Cosnote.css';
@@ -21,14 +21,14 @@ class Cosnote extends Component {
         this.state = {
             user: null,
             isLoading: false,
-            apiErrorMessage: String(),
+            errorMessage: String(),
         };
         this.request = this.request.bind(this);
     }
 
     // showErrorMessageBar(message) {
     //     this.setState(prevState => {
-    //         prevState.apiErrorMessage = message;
+    //         prevState.errorMessage = message;
     //         return prevState
     //     })
     // }
@@ -51,11 +51,26 @@ class Cosnote extends Component {
         return fetch(this.API_BASE_URL + route, options);
     };
 
-    authorizeWithSession() {
+    startLoading(start=true) {
+        this.setState(state => {
+            return {...state, isLoading: start}
+        })
+    }
 
+    authorizeWithSession() {
+        this.startLoading();
+        this.request("/user/", {method: "GET"}).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    this.setState(state => {return {...state, user: json}});
+                })
+            }
+            this.startLoading(false);
+        })
     }
 
     componentDidMount() {
+        this.authorizeWithSession();
     };
 
     render() {
@@ -64,7 +79,7 @@ class Cosnote extends Component {
                 <Fabric className="workspace">
                     <div className="workspace">
                         <Header cosnote={this} />
-                        {/* {this.state.user ? <Main cosnote={this} /> : <Login cosnote={this} />} */}
+                        {this.state.user ? <Main cosnote={this} /> : <Login cosnote={this} />}
                         <Main cosnote={this} />
                     </div>
                 </Fabric>
