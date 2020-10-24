3fcf80280da281a8f440f4d890077c8d922b2f79
thec0sm0s <deepakrajko14@gmail.com>
2020-10-24 23:35:17 +0530

    Add base action component EditNote with search bar

--- src/components/main/ActionBar.jsx
+++ src/components/main/ActionBar.jsx
@@ -1,4 +1,4 @@
-import { DefaultEffects } from '@fluentui/react'
+import { DefaultEffects, Stack, TextField} from '@fluentui/react'
 import React, { Component } from 'react'
 import { CosnoteTheme } from "../../cosnoteTheme";
 
@@ -7,10 +7,38 @@ const sideActionBarStyles = {
     background: CosnoteTheme.palette.neutralLighter,
 }
 
+const stackItemStyles = {
+    root: {
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+    }
+}
+
+const primaryItemStyles = {
+    root: {
+        ...stackItemStyles.root,
+        width: "100%",
+    }
+}
+
+class EditNote extends Component {
+    render() {
+        return (
+            <Stack horizontalAlign="center" styles={{root: {height: "100%", padding: 20}}}>
+                <Stack.Item disableShrink styles={primaryItemStyles}>
+                    <TextField className="noteTitleField" placeholder="Title of this note or script." iconProps={{iconName: "ChevronRightMed"}} style={{backgroundColor: CosnoteTheme.palette.neutralLighter}} underlined />
+                </Stack.Item>
+            </Stack>
+        )
+    }
+}
+
 export default class ActionBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            "editNote": <EditNote />,
         }
     }
 
@@ -18,7 +46,7 @@ export default class ActionBar extends Component {
         return (
             <>
                 <div className="sideActionBar" style={sideActionBarStyles}>
-
+                    {this.state[this.props.context.type]}
                 </div>
             </>
         )
